86752d204ae56294b3e7b0ace07966a51ce03da0
Gabriel <[gabrielgamboav19@gmail.com]>
2023-04-04 00:09:26 -0600

    Campus.hpp terminado

--- Tarea 1/source/campus.hpp
+++ Tarea 1/source/campus.hpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <list>
+#include <string>
 using namespace std;
 
 class Edificio {
 
     private:
-        bool ascensor;
-
+        bool ascensor;  
 };
 
 class EdificioDeAulas : public Edificio {
@@ -14,7 +14,9 @@ class EdificioDeAulas : public Edificio {
     private:
         bool soda;
         list<Aula> aulas;
-
+    
+    public:
+        EdificioDeAulas (bool soda, list<Aula> aulas);
 };
 
 class Aula {
@@ -23,11 +25,11 @@ class Aula {
         int id;
         int pupitres;
         bool proyector;
+
+    Aula (int id, int pupitres, bool proyector);
 };
 
 class Parqueo : public Edificio {
-
-
 };
 
 class Finca {
@@ -35,12 +37,19 @@ class Finca {
     private:
         bool bus_interno;
         list<Edificio> edificios; 
+        string nombre;
 
+    public:
+        Finca(bool bus_interno, string nombre, list<Edificio> edificios);
 };
 
 class Campus {
 
     private:
         Finca fincas [5];
+        int indice;
 
+    public:
+    
+        Campus (Finca* fincas);
 };
