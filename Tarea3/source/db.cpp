c6931a326e3960e43db3989550f5d234c4af9033
Gabriel <[gabrielgamboav19@gmail.com]>
2023-05-18 00:21:33 -0600

    funciones para buscar terminadas | tarea 3

--- Tarea3/source/db.cpp
+++ Tarea3/source/db.cpp
@@ -69,17 +69,19 @@ vector<Empleado> buscarPorDepartamento(vector<Empleado> empleados, string depart
     return empleadosEnDepartamento;
 }
 
-vector<Empleado> buscarPorSalario(vector<Empleado> empleados, double salario) {
+vector<Empleado> buscarPorSalario(vector<Empleado> empleados, double salarioMinimo, double salarioMaximo) {
     vector<Empleado> empleadosConSalario;
 
-    // Validar la entrada del usuario
-    if (!validarEntrada(to_string(salario), "^\\$?[0-9]+(\\.[0-9]+)?$")) {
+    // Validar las entradas del usuario
+    if (!validarEntrada(to_string(salarioMinimo), "^\\$?[0-9]+(\\.[0-9]+)?$") || 
+        !validarEntrada(to_string(salarioMaximo), "^\\$?[0-9]+(\\.[0-9]+)?$")) {
         cout << "Entrada de salario inválida." << endl;
         return empleadosConSalario;
     }
 
     for (Empleado empleado : empleados) {
-        if (empleado.getSalario() == salario) {
+        double salario = empleado.getSalario();
+        if (salario >= salarioMinimo && salario <= salarioMaximo) {
             empleadosConSalario.push_back(empleado);
         }
     }
@@ -87,26 +89,61 @@ vector<Empleado> buscarPorSalario(vector<Empleado> empleados, double salario) {
     return empleadosConSalario;
 }
 
+
 int main() {
+    // Se declara una variable "nombreArchivo" y se le asigna el valor "empleados.csv"
     string nombreArchivo = "empleados.csv";
 
+    // Se declara un vector de empleados llamado "empleados" y se inicializa llamando a la función "leerArchivo" pasando el nombre del archivo como parámetro
     vector<Empleado> empleados = leerArchivo(nombreArchivo);
+
+    // Se verifica si el vector de empleados está vacío
     if (empleados.empty()) {
+        // Si está vacío, se muestra un mensaje de que no se encontraron empleados en el archivo
         cout << "No se encontraron empleados en el archivo." << endl;
+        // Se retorna el valor 1 para indicar un error
         return 1;
     }
 
+    // Se muestra un encabezado indicando que se van a imprimir los empleados leídos del archivo
     cout << "Empleados leídos del archivo:" << endl;
+
+    // Se itera sobre cada objeto "Empleado" en el vector "empleados"
     for (Empleado empleado : empleados) {
+        // Se imprime el nombre, departamento y salario de cada empleado
         cout << empleado.getNombre() << " - " << empleado.getDepartamento() << " - " << empleado.getSalario() << endl;
     }
 
-    string departamento = "Tecnologia12";
+    // Se declara una variable "departamento" y se le asigna el valor "Tecnologia"
+    string departamento = "Tecnologia";
+
+    // Se declara un nuevo vector de empleados llamado "empleadosEnTecnologia" y se llama a la función "buscarPorDepartamento" pasando el vector "empleados" y el departamento como parámetros
     vector<Empleado> empleadosEnTecnologia = buscarPorDepartamento(empleados, departamento);
+
+    // Se muestra un encabezado indicando que se van a imprimir los empleados en el departamento especificado
     cout << "\nEmpleados en el departamento de " << departamento << ":" << endl;
+
+    // Se itera sobre cada objeto "Empleado" en el vector "empleadosEnTecnologia"
     for (Empleado empleado : empleadosEnTecnologia) {
+        // Se imprime el nombre de cada empleado en el departamento especificado
         cout << empleado.getNombre() << endl;
     }
 
+    // Se declaran dos variables "salarioMinimo" y "salarioMaximo" y se les asigna un valor numérico
+    double salarioMinimo = 5000;
+    double salarioMaximo = 6000;
+
+    // Se declara un nuevo vector de empleados llamado "empleadosEnRangoSalario" y se llama a la función "buscarPorSalario" pasando el vector "empleados", "salarioMinimo" y "salarioMaximo" como parámetros
+    vector<Empleado> empleadosEnRangoSalario = buscarPorSalario(empleados, salarioMinimo, salarioMaximo);
+
+    // Se muestra un encabezado indicando que se van a imprimir los empleados dentro del rango salarial especificado
+    cout << "\nEmpleados con salario entre " << salarioMinimo << " y " << salarioMaximo << ":" << endl;
+    // Se itera sobre cada objeto "Empleado" en el vector "empleadosEnRangoSalario"
+    for (Empleado empleado : empleadosEnRangoSalario) {
+        // Se imprime el nombre y salario de cada empleado dentro del rango especificado
+        cout << empleado.getNombre() << " - " << empleado.getSalario() << endl;
+    }
+    
     return 0;
 }
+
